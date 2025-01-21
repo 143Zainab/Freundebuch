989c17a0044065524eb7a1a9ae965ebd53922b49
143Zainab <ahmedzainab572@gmail.com>
2025-01-21 15:02:15 +0100

    Update Freundebuch.java

--- Freundebuch.java
+++ Freundebuch.java
@@ -195,31 +195,5 @@ public class Freundebuch {
         frame.setVisible(true);
     }
 
-    // Klasse für Freund-Objekte
-    private static class Freund {
-        private String name;
-        private String geburtsdatum;
-        private String sternzeichen;
-        private boolean single;
-        private String augenfarbe;
-        private String superkraft;
 
-        public Freund(String name, String geburtsdatum, String sternzeichen, boolean single, String augenfarbe, String superkraft) {
-            this.name = name;
-            this.geburtsdatum = geburtsdatum;
-            this.sternzeichen = sternzeichen;
-            this.single = single;
-            this.augenfarbe = augenfarbe;
-            this.superkraft = superkraft;
-        }
-
-        public String getSuperkraft() {
-            return superkraft;
-        }
-
-        public String toString() {
-            return name + ", geboren am " + geburtsdatum + " (" + sternzeichen + "), Single: " + (single ? "Ja" : "Nein")
-                    + ", Augenfarbe: " + augenfarbe + ", Superkraft: " + superkraft;
-        }
-    }
 }
